1445e6f13a3fbb755a315402984e35faa8ab709c
Dan Appel <dan.appel00@gmail.com>
2016-06-09 00:21:36 -0700

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Resource",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 7),
     ]
 )
